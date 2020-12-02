35c51edd3d6b3db7739b69fd82ed8856c691f038
Sam Robbins <samrobbinsgb@gmail.com>
2020-12-02 20:34:29 +0000

    Fix multiple react issue

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,19 @@
 import resolve from "rollup-plugin-node-resolve";
 import babel from "rollup-plugin-babel";
 import commonjs from "rollup-plugin-commonjs";
-import react from "react";
-import reactDom from "react-dom";
 export default {
   input: "./index.js",
   output: {
     file: "./output.js",
     format: "cjs",
   },
+  external: ["react", "react-dom"],
   plugins: [
     babel({
       exclude: "node_modules/**",
     }),
     resolve(),
 
-    commonjs({
-      include: "node_modules/**",
-      namedExports: {
-        react: Object.keys(react),
-        "react-dom": Object.keys(reactDom),
-      },
-    }),
+    commonjs(),
   ],
 };
