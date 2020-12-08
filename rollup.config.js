d8ab120f4f629f88730afa5c68b913a3e1142c86
Sam Robbins <samrobbinsgb@gmail.com>
2020-12-08 15:43:01 +0000

    Change to use ES Modules and newer rollup packages

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,20 @@
-import resolve from "rollup-plugin-node-resolve";
-import babel from "rollup-plugin-babel";
-import commonjs from "rollup-plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import babel from "@rollup/plugin-babel";
 import postcss from "rollup-plugin-postcss";
 export default {
   input: "./index.js",
   output: {
     file: "./output.js",
-    format: "cjs",
+    format: "esm",
   },
   external: ["react", "react-dom"],
   plugins: [
     babel({
+      babelHelpers: "bundled",
       exclude: "node_modules/**",
     }),
     resolve(),
 
-    commonjs(),
     postcss({
       config: {
         path: "./postcss.config.js",
