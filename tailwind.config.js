4229bf04d98480c8cf3129779c8026677f405191
Sam Robbins <samrobbinsgb@gmail.com>
2020-12-10 15:48:28 +0000

    Start dependency upgrade for postcss 8, but storybook doesn't support

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ const colors = require("tailwindcss/colors");
 module.exports = {
   purge: ["./components/**/*.js", "./stories/**/*.*", "./.storybook/*.js"],
   darkMode: false,
+
   theme: {
     extend: {
       colors: {
