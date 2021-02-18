148b6bd8a216636adeaa68f427c469c0ba22dd08
Sam Robbins <samrobbinsgb@gmail.com>
2021-02-18 11:29:40 +0000

    Add name to corollary

--- components/corollary.js
+++ components/corollary.js
@@ -4,7 +4,7 @@ export default function corollary(props) {
   return (
     <div className="border border-orange-400 rounded p-4 mx-2 my-6 bg-orange-100">
       <span className="block text-2xl font-semibold text-orange-600">
-        Corollary
+        Corollary {props.name ? "- " + props.name : ""}
       </span>
       <div>{props.children}</div>
     </div>
